953274541eb9971c477955af2596a881c3c09d80
hlc23 <henryleecode23@gmail.com>
2025-11-11 13:31:59 +0800

    add auto from CSRF attack

--- csrf-demo/attacker/app.py
+++ csrf-demo/attacker/app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/fetch-attack')
+def fetch_attack():
+    return render_template('fetch_attack.html')
+
+@app.route('/form-attack')
+def form_attack():
+    return render_template('form_attack.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
